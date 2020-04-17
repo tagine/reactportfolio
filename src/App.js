e7be85aa60eeaa0d78adccaa37fbef9e50d742b8
tagine <rana@ranafayez.com>
2020-04-17 17:32:34 -0400

    semantic UI added and contact form

--- src/App.js
+++ src/App.js
@@ -1,15 +1,19 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { Header } from 'semantic-ui-react';
+import Contactform from "./components/Contactform.jsx";
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
+      <Header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Welcome to my Website
         </p>
+        <Contactform/>
         <a
           className="App-link"
           href="https://reactjs.org"
@@ -18,7 +22,7 @@ function App() {
         >
           Learn React
         </a>
-      </header>
+      </Header>
     </div>
   );
 }
