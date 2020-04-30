6c1f6616db017459cec401ea7eb01e418b81405e
tagine <rana@ranafayez.com>
2020-04-30 17:12:33 -0400

    components defined

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,24 +1,24 @@
-import React from 'react';
-import { Header, Container, Image } from 'semantic-ui-react';
+// import React from 'react';
+// import { Header, Container, Image } from 'semantic-ui-react';
 
 
-export default Header () => (
-    <Menu>
-      <Container>
-        <Menu.Item as="a" header>
-          <Image
-            size="small"
-            src="/img/page/logo.svg"
-          />
-        </Menu.Item>
-        <Menu.Menu position="right">
-          <Menu.Item as="a" name="login">
-            Login
-          </Menu.Item>
-          <Menu.Item as="a" name="register">
-            Register
-          </Menu.Item>
-        </Menu.Menu>
-      </Container>
-    </Menu>
-  );
+// export default Header () => (
+//     <Menu>
+//       <Container>
+//         <Menu.Item as="a" header>
+//           <Image
+//             size="small"
+//             src="/img/page/logo.svg"
+//           />
+//         </Menu.Item>
+//         <Menu.Menu position="right">
+//           <Menu.Item as="a" name="login">
+//             Login
+//           </Menu.Item>
+//           <Menu.Item as="a" name="register">
+//             Register
+//           </Menu.Item>
+//         </Menu.Menu>
+//       </Container>
+//     </Menu>
+//   );
