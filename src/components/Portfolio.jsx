ef2a864bb66288dd49b1fc683b0b41db11801e58
tagine <rana@ranafayez.com>
2020-06-22 17:19:16 -0400

    card link experiment completed links back to normal now with errors fixed

--- src/components/Portfolio.jsx
+++ src/components/Portfolio.jsx
@@ -32,28 +32,28 @@ const Portfolio = (props) => (
                     title="Pooch Quest"
                     tech="HTML, CSS, Materialize, Javascript, jQuery, TheDog API, and PetFinder API."
                     description="Pooch Quest is an application that assists the user in finding their perfect dog. As soon as the user searches, Pooch Quest gives relevant information about their desired breed along with adoptable dogs in their area."
-                    //applink="https://acucunato.github.io/pooch-quest/"
+                    applink="https://acucunato.github.io/pooch-quest/"
                     github="https://github.com/tagine/pooch-quest"/>
                 <ProjectCard
                     screenshot={employeedirectory}
                     title="Employee Directory"
                     tech="React, Semantic UI, and Express."
                     description="Employee Directory is a React app that sorts through employee salary data."
-                    //applink="https://arcane-temple-83433.herokuapp.com/"
+                    applink="https://arcane-temple-83433.herokuapp.com/"
                     github="https://github.com/tagine/employeedirectory2"/>
                 <ProjectCard
                     screenshot={budgettracker}
                     title="Budget Tracker"
                     tech="HTML, CSS, Express, Javascript."
                     description="This budget tracker application can help you balance your recurring expenses and income."
-                    //applink="http://offlinebudgets.herokuapp.com/"
+                    applink="http://offlinebudgets.herokuapp.com/"
                     github="https://github.com/tagine/offlinebudget"/>
                 <ProjectCard
                     screenshot={nytsearch}
                     title="NY Times Seach Engine"
                     tech="HTML, CSS, Ajax, and Javascript."
                     description="This is a search engine created exclusively for pulling articles from the New York Times. Designed in CSS."
-                    //applink="https://tagine.github.io/searchengine/"
+                    applink="https://tagine.github.io/searchengine/"
                     github="https://github.com/tagine/searchengine"/>
             </Card.Group>
         {/* </BrowserRouter> */}
