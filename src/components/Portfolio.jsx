468329e9df6df3b444fe8e365dafc8f1f48bb4fd
tagine <rana@ranafayez.com>
2020-06-22 17:14:49 -0400

    links updated again

--- src/components/Portfolio.jsx
+++ src/components/Portfolio.jsx
@@ -25,35 +25,35 @@ const Portfolio = (props) => (
                     title="B. There."
                     tech="Eventful API, jQuery, Express server, and Materialize CSS"
                     description="A calendar built in an MVC model. The goal behind this application is to create a space where a user can store all of the events they're interested in."
-                    applink="https://github.com/tagine/space"
-                    github="https://fierce-temple-54990.herokuapp.com/" />
+                    applink="https://fierce-temple-54990.herokuapp.com/"
+                    github="https://github.com/tagine/space"/>
                 <ProjectCard
                     screenshot={poochquest}
                     title="Pooch Quest"
                     tech="HTML, CSS, Materialize, Javascript, jQuery, TheDog API, and PetFinder API."
                     description="Pooch Quest is an application that assists the user in finding their perfect dog. As soon as the user searches, Pooch Quest gives relevant information about their desired breed along with adoptable dogs in their area."
-                    applink="https://acucunato.github.io/pooch-quest/"
+                    //applink="https://acucunato.github.io/pooch-quest/"
                     github="https://github.com/tagine/pooch-quest"/>
                 <ProjectCard
                     screenshot={employeedirectory}
                     title="Employee Directory"
                     tech="React, Semantic UI, and Express."
                     description="Employee Directory is a React app that sorts through employee salary data."
-                    applink="https://arcane-temple-83433.herokuapp.com/"
+                    //applink="https://arcane-temple-83433.herokuapp.com/"
                     github="https://github.com/tagine/employeedirectory2"/>
                 <ProjectCard
                     screenshot={budgettracker}
                     title="Budget Tracker"
                     tech="HTML, CSS, Express, Javascript."
                     description="This budget tracker application can help you balance your recurring expenses and income."
-                    applink="http://offlinebudgets.herokuapp.com/"
+                    //applink="http://offlinebudgets.herokuapp.com/"
                     github="https://github.com/tagine/offlinebudget"/>
                 <ProjectCard
                     screenshot={nytsearch}
                     title="NY Times Seach Engine"
                     tech="HTML, CSS, Ajax, and Javascript."
                     description="This is a search engine created exclusively for pulling articles from the New York Times. Designed in CSS."
-                    applink="https://tagine.github.io/searchengine/"
+                    //applink="https://tagine.github.io/searchengine/"
                     github="https://github.com/tagine/searchengine"/>
             </Card.Group>
         {/* </BrowserRouter> */}
